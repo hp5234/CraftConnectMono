7098364d2e596e60bbff86f083470cb5103803bb
jeonjinwoo <wls45al@gmail.com>
2025-06-22 22:27:23 +0900

    first commit

---
+++ src/main/java/com/craftconnect/craftconnectmono/CraftconnectmonoApplication.java
@@ -0,0 +1,13 @@
+package com.craftconnect.craftconnectmono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftconnectmonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftconnectmonoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/craftconnect/craftconnectmono/CraftconnectmonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craftconnect.craftconnectmono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftconnectmonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
